c74578c3ea5419b46fd85eeb3c56046551321b6b
Irina <izhts1482@gmail.com>
2023-11-15 19:22:13 +0300

    Task 1

--- src/test/java/ru/netology/web/DebitCardFormTest.java
+++ src/test/java/ru/netology/web/DebitCardFormTest.java
@@ -2,22 +2,24 @@ package ru.netology.web;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DebitCardFormTest {
     private WebDriver driver;
 
     @BeforeAll
-    public static void setup() {
+    public static void setupAll() {
         // System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     public void BeforeEach() {
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -31,22 +33,14 @@ public class DebitCardFormTest {
         driver = null;
     }
 
-
     @Test
-    void shouldTestPositiveValues() throws InterruptedException {
-       // throw new UnsupportedOperationException();
+    void shouldTestPositiveValues() {
         driver.get("http://localhost:9999");
-        Thread.sleep(5000);
-
-
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванова Ольга");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79190006070");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
-//        open("http://localhost:9999");
-//        $("[data-test-id=name] input").setValue("Ольга");
-//        $("[data-test-id=phone] input").setValue("+79190006070");
-//        $("[data-test-id=agreement]").click();
-//        $("[data-test-id=submit]").click();
-//        $(".button__content").click();
-//        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-//        // button.button
-//    }
 }
